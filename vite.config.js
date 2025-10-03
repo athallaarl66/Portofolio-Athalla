28cec3c4548c737a2c379fface207dcec225de8f
Athalla <athallaarli@gmail.com>
2025-10-03 17:21:49 +0700

    ubah vite sama install lightinig css

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// disini tambah resolve alias biar path import tinggal panggil folder sama nama file pake @
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -11,5 +10,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  css: {
+    transformer: "postcss",
   },
 });
