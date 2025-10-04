de51d4ee269aebf4bbe2a91575b5a7dd803992f8
Athalla <athallaarli@gmail.com>
2025-10-04 19:13:16 +0700

    mindahin file cv sama ubah cv aboutsection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -59,8 +59,10 @@ export const AboutSection = () => {
                 Contact
               </a>
               <a
-                href="/public/CV_Athalla Arli Abhinaya_English.pdf"
+                href="/projects2/CV_Athalla Arli Abhinaya_English.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 Download CV
               </a>
